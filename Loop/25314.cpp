7c2c35b0ecf8be682e465cafdcd4249d3517d5bf
Haeo <77920991+Haeo@users.noreply.github.com>
2023-10-04 16:13:24 +0900

    Update and rename 25314.cpp to Loop/25314.cpp

---
+++ Loop/25314.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	int result = 0;
+	cin >> n;
+	if (n % 4 == 0) {
+		result = n / 4;
+	}
+	else {
+		result = n / 4 + 1;
+	}
+	for (int i = 0; i < result; i++) {
+		cout << "long ";
+	}
+	cout << "int";
+
+	return 0;
+}
