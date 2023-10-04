bb7b0fdee1bd74d3024f289dfc0ef9c7f706af31
Haeo <77920991+Haeo@users.noreply.github.com>
2023-10-04 11:51:41 +0900

    Create 11005.cpp

---
+++ General Math 1/11005.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main() {
+	int n, b;
+	cin >> n >> b;
+	string v = "";
+	string arr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	while (n > 0) {
+		v.push_back(arr[n % b]);
+		n /= b;
+	}
+	reverse(v.begin(), v.end());
+	cout << v;
+
+	return 0;
+}
