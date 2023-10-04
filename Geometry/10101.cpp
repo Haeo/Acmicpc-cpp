5bf18eec8f53be3d6d1c9d49da44ec1c9fce8750
Haeo <77920991+Haeo@users.noreply.github.com>
2023-10-04 11:42:06 +0900

    Create 10101.cpp

---
+++ Geometry/10101.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <set>
+using namespace std;
+int main() {
+	int num;
+	int sum = 0;
+	set<int> s;
+	for (int i = 0; i < 3; i++) {
+		cin >> num;
+		sum += num;
+		s.insert(num);
+	}
+	if (s.size() == 1 && s.end() != s.find(60) && sum == 180) {
+		cout << "Equilateral";
+	}
+	else if (sum == 180 && s.size() == 2) {
+		cout << "Isosceles";
+	}
+	else if (sum == 180 && s.size() == 3) {
+		cout << "Scalene";
+	}
+	else if (sum != 180) {
+		cout << "Error";
+	}
+
+	return 0;
+}
