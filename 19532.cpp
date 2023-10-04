1da215930d813a08eba662716adbb4a1f2ee2bba
Haeo <77920991+Haeo@users.noreply.github.com>
2023-10-04 11:27:45 +0900

    Delete 19532.cpp

--- 19532.cpp
+++
@@ -1,9 +0,0 @@
-//#include <iostream>
-//using namespace std;
-//int main() {
-//	int a, b, c, d, e, f;
-//	cin >> a >> b >> c >> d >> e >> f;
-//	cout << (b * f - c * e) / (b * d - a * e) << " ";
-//	cout << (a * f - c * d) / (a * e - b * d);
-//	return 0;
-//}
