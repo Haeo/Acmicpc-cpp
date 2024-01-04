840a266a22711472bdeb4e53e785da98d4c92f05
Haeo <77920991+Haeo@users.noreply.github.com>
2024-01-04 21:28:40 +0900

    Upload 27433.cpp

---
+++ Recursion/27433.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+long factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    long result = factorial(n);
+    cout << result << "\n";
+    return 0;
+}
