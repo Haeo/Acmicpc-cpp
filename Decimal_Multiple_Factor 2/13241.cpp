ff7241905b9335efd4d3ac0fbe0a94e32d4c459b
Haeo <77920991+Haeo@users.noreply.github.com>
2024-01-05 23:48:19 +0900

    Upload 13241.cpp

---
+++ Decimal_Multiple_Factor 2/13241.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int gcd(long long int a, long long int b) {
+    long long int r;
+    while (b != 0) {
+        r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+long long int lcm(long long int a, long long int b) {
+    return (a * b) / gcd(a, b);
+}
+
+int main() {
+    long long int a, b;
+    cin >> a >> b;
+    cout << lcm(a, b);
+
+    return 0;
+}
